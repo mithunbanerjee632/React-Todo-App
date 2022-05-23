1b59b4c9854d72b331d9d1cbd356b1428ae92c05
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-05-23 14:35:01 +0600

    form value controlled by using useState()

--- src/component/Newtodo.js
+++ src/component/Newtodo.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React,{useState} from 'react';
 import style from './css/Newtodo.module.css';
 
 const Newtodo = () => {
+
+  const [todo,setTodo]=useState({title:" ", description:" "});
+
+  const {title,description}=todo
+
+  const handleChange=(event)=>{
+     const name=event.target.name;
+
+     setTodo((oldTodo)=>{
+         return {...oldTodo,[name]:event.target.value}
+     })
+  }
+
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+    console.log(todo);
+    setTodo({title:" ",description:" "});
+  }
   return (
     
-      <form className={style.form}>
+      <form className={style.form} onSubmit={handleSubmit}>
         <div className={style["form-field"]}>
           <label htmlFor='title'>Title: </label>
-          <input type="text" name="title" id="title"/>
+          <input type="text" name="title" id="title" onChange={handleChange} value={title}/>
         </div>
 
         <div className={style["form-field"]}>
           <label htmlFor='description'>Description: </label>
-          <textarea type="text" name="description" id="description"/>
+          <textarea type="text" name="description" id="description" onChange={handleChange} value={description}/>
         </div>
 
         <button type='submit'>Add Todo</button>
